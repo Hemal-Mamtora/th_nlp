ddfea62fd87205e57129845c0e7c72c89121c232
Hemal-Mamtora <hemalmamtora1@gmail.com>
2022-01-29 15:24:27 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import fitz
+
+path = "/home/hemal/th/nlp/training_invoices/training/Invoice 630559.pdf"
+
+doc = fitz.open(path)
+
+for page in doc:  # iterate the document pages
+    textpage = page.get_textpage()
+    text = textpage.extractWORDS()  # get plain text (is in UTF-8)
+    print(text)  # write text of page
